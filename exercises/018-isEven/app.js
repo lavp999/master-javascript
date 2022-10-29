021333cc49cd6b1d79c300a6e6d1b93c34e51c05
lavp999 <lavp99@hotmail.com>
2022-10-29 20:40:07 +0000

    Hasta ejercicio 20

--- exercises/018-isEven/app.js
+++ exercises/018-isEven/app.js
@@ -1,1 +1,7 @@
 // Write your function here
+function isEven(n){
+  return (n%2)==0;
+}
+
+let output = isEven(10);
+console.log(output); // --> falso
--- exercises/019-isOdd/app.js
+++ exercises/019-isOdd/app.js
@@ -1,1 +1,7 @@
 // Write your function here
+function isOdd(n){
+    return ((n%2) != 0);
+  }
+  
+  let output = isOdd(11);
+  console.log(output); // --> falso
