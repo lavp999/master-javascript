d2da5967e48f629e344e9673afc6e1fab67a9dc5
lavp999 <lavp99@hotmail.com>
2022-10-30 17:06:14 +0000

    Hasta el 40

--- exercises/035-isPersonOldEnoughToVote/app.js
+++ exercises/035-isPersonOldEnoughToVote/app.js
@@ -1,4 +1,10 @@
 function isPersonOldEnoughToVote(person) {
   // Add your code after this line
-  
+  return (person.age >= 21);
 }
+
+let obj = {
+  age: 16
+};
+let output = isPersonOldEnoughToDrive(obj);
+console.log(output); // --> true
--- exercises/036-isPersonOldEnoughToDrink/app.js
+++ exercises/036-isPersonOldEnoughToDrink/app.js
@@ -1,4 +1,10 @@
 function isPersonOldEnoughToDrink(person) {
   // Add your code after this line
-  
+  return (person.age >= 21);
 }
+
+let obj = {
+  age: 16
+};
+let output = isPersonOldEnoughToDrink(obj);
+console.log(output); // --> false
