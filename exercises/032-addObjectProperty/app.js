b271a587c2228742cc4631668d6274f61e7f4c34
lavp999 <lavp99@hotmail.com>
2022-10-30 12:51:33 +0000

    ejercicio 34

--- exercises/032-addObjectProperty/app.js
+++ exercises/032-addObjectProperty/app.js
@@ -1,4 +1,16 @@
 function addObjectProperty(obj1, key, obj2) {
   // Add your code after this line
-
+  obj1[key] = obj2;
+  return obj1;
 }
+
+let person1 = {
+  name: 'Joe Blow',
+  role: 'schlub'
+};
+let person2 = {
+  name: 'Mr. Burns',
+  role: 'supervisor'
+};
+
+console.log(addObjectProperty( person1, "hijo", person2));
--- exercises/033-isPersonOldEnoughToDrinkAndDrive/app.js
+++ exercises/033-isPersonOldEnoughToDrinkAndDrive/app.js
@@ -1,5 +1,12 @@
 function isPersonOldEnoughToDrinkAndDrive(person) {
   // the person object contains an "age" property inside
   // Add your code after this line
+  return (person.age >= 21); // si tiene edad para beber tiene para conducir
   
 }
+
+let obj = {
+  age: 45
+};
+let output = isPersonOldEnoughToDrinkAndDrive(obj);
+console.log(output); // --> false
