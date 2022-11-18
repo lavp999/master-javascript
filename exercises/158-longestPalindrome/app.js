7002e66faa32e226604cffafd8e175fec8d9d077
lavp999 <lavp99@hotmail.com>
2022-11-18 13:06:43 +0000

    158 hecho

--- exercises/158-longestPalindrome/app.js
+++ exercises/158-longestPalindrome/app.js
@@ -3,56 +3,40 @@ function findLongestPalindrome(sentence) {
   // iterate words and collect the palindromes
   // sort the list of palindromes by word length
   // return the largest item in the sorted list
-
-   // dividir la oración en palabras
-   // iterar palabras y recoger los palíndromos
-   // ordenar la lista de palíndromos por longitud de palabra
-   // devuelve el elemento más grande en la lista ordenada
     let miFrase = sentence.toLowerCase();
     let arrPalabras = [];
-    let longPalabras = 3;
-    let maxFor = miFrase.length-longPalabras;
+    let longPalabras;// = 3;
+    let maxFor; // = miFrase.length-longPalabras;
+    let palabraMax = 0;
 
-      for(x=0; x < maxFor; x++){
-        //console.log(miFrase.substr(x,x+longPalabras));
-        console.log(x , x+longPalabras-1);
-        arrPalabras.push(miFrase.substr(x,longPalabras));
+      for (longPalabras=3; longPalabras<=miFrase.length;longPalabras++){
+        maxFor = miFrase.length-longPalabras+1;
+        for(x=0; x < maxFor; x++)
+            if (isPalindrome(miFrase.substr(x,longPalabras)))
+                arrPalabras.push(miFrase.substr(x,longPalabras));
       }
 
-    return arrPalabras;
+      arrPalabras.forEach((elemento, index) => {
+        if(elemento.length >= arrPalabras[palabraMax].length) palabraMax = index;
+      });
+
+      // console.log(arrPalabras);
+
+    return arrPalabras[palabraMax];
 }
 
 function reverseString(string) {
   let arrStr = string.split("");
   let cadena = arrStr.reverse().join("");
 
-  let arr = [];
-  for(let i of cadena) {
-      arr.push(i);
-  }
-
   return cadena;
-
 }
   
 function isPalindrome(word) {
   // hint: you can detect palindromes by comparing a string to its reverse
-
-  return word;
-
+  return (word === reverseString(word));
 }
 
+let output = findLongestPalindrome("My dad is a racecar athlete");
+console.log(output);
 
-console.log("-------------------------------------------------------------");
-// let output = findLongestPalindrome("My dad is a racecar athlete");
-let output = reverseString("My dad is a racecar athlete");
-console.log("reverseString: ", output);
-
-output = findLongestPalindrome(output);
-console.log("findLongestPalindrome: ", output);
-
-
-output = isPalindrome(output);
-console.log("isPalindrome: ", output);
-console.log("-------------------------------------------------------------");
-console.log("");console.log("");console.log("");
