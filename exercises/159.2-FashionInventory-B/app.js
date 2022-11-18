ba93808d2cb62ed02904f0ec6398507dfc2ffcf4
lavp999 <lavp99@hotmail.com>
2022-11-18 18:16:41 +0000

    159-2 casi

--- exercises/159.2-FashionInventory-B/app.js
+++ exercises/159.2-FashionInventory-B/app.js
@@ -19,4 +19,30 @@ let currentInventory = [
 
 function renderAverageCostPerDesigner(inventory) {
     // your code here
+    let miArray = [];
+    let diseAnterior = inventory[0].name;
+    let suma = 0;
+    let cont = 1;
+
+      inventory.forEach((disenadores, indDis) => {
+          disenadores.shoes.forEach((zapatos, index) => {
+              console.log(disenadores.name, ' - ', zapatos.price, ' - ',diseAnterior);
+              if (disenadores.name != diseAnterior){
+                  // miArray.push({"name": diseAnterior, "averagePrice" : suma/cont});
+                  suma = 0;
+                  cont = 1;
+              }else{
+                 suma += zapatos.price;
+                  cont += 1;
+              }
+              diseAnterior = disenadores.name;
+          });
+      });
+
+    return miArray;
+
 }
+
+console.log("-----------------------------------------------------")
+console.log("mi array", renderAverageCostPerDesigner(currentInventory))
+console.log("-----------------------------------------------------")
