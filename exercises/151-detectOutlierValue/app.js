a97af9bbbe962acafb18a741cb13b9c6f5e82065
lavp999 <lavp99@hotmail.com>
2022-11-21 22:55:35 +0000

    terminado con 151

--- exercises/151-detectOutlierValue/app.js
+++ exercises/151-detectOutlierValue/app.js
@@ -1,10 +1,24 @@
 function detectOutlierValue(string) {
     // your code here
+    let arrBinario = string.split(' ').map((e)=>{ return parseInt(e)%2 == 0 ? 0 :1 }); 
+    let posPar = 0;
+    let posImpar = 0;
+    let par = 0;
+    let impar = 0;
 
-    // --------------------
-    // no lo entiendo
-    // --------------------
+        arrBinario.forEach((e, i) => {
+            if(e == 0){
+                posPar = i;
+                par++;
+            }else{
+                posImpar = i;
+                impar++;
+            } 
+        });
+        if (par > impar) return posImpar+1;
+        else             return posPar+1;    
 }
+ 
 
 // Third number is odd, while the rest of the numbers are even
 console.log(detectOutlierValue("2 4 7 8 10")); // => 3 
